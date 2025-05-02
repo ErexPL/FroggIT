355276af7c204ab5c717baa447033f56e83a19e5
ErexPL <zys.dawid@gmail.com>
2025-05-02 16:17:32 +0200

    Refactor HTML structure and styles; remove main.css, add style.css and Font Awesome for improved design and theme toggle functionality

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+const themeToggle = document.querySelector('.theme-toggle');
+let darkMode = localStorage.getItem('darkMode') === 'true';
+
+const updateTheme = () => {
+    if (darkMode) {
+        themeToggle.querySelector('i').classList.replace('fa-sun', 'fa-moon');
+        document.documentElement.style.setProperty('--white-color', '#18181f');
+        document.documentElement.style.setProperty('--light-color', '#080810');
+        document.documentElement.style.setProperty('--dark-color', '#f8ffff');
+        document.documentElement.style.setProperty('--shadow-color', 'rgba(0, 0, 0, 0.5)');
+    } else {
+        themeToggle.querySelector('i').classList.replace('fa-moon', 'fa-sun');
+        document.documentElement.style.setProperty('--white-color', '#f8ffff');
+        document.documentElement.style.setProperty('--light-color', '#e0f0f0');
+        document.documentElement.style.setProperty('--dark-color', '#18181f');
+        document.documentElement.style.setProperty('--shadow-color', 'rgba(0, 0, 0, 0.1)');
+    }
+};
+
+if (themeToggle) {
+    updateTheme();
+
+    themeToggle.addEventListener('click', () => {
+        darkMode = !darkMode;
+        localStorage.setItem('darkMode', darkMode);
+        updateTheme();
+    });
+}
