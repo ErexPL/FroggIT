4eecce2367f88eca62328a8e02e6be5c97ee1aa5
ErexPL <zys.dawid@gmail.com>
2025-05-02 23:36:19 +0200

    Brand new footer + color improvements

--- public/js/main.js
+++ public/js/main.js
@@ -4,15 +4,15 @@ let darkMode = localStorage.getItem('darkMode') === 'true';
 const updateTheme = () => {
     if (darkMode) {
         themeToggle.querySelector('i').classList.replace('fa-sun', 'fa-moon');
-        document.documentElement.style.setProperty('--white-color', '#18181f');
-        document.documentElement.style.setProperty('--light-color', '#080810');
+        document.documentElement.style.setProperty('--white-color', '#282830');
+        document.documentElement.style.setProperty('--light-color', '#181810');
         document.documentElement.style.setProperty('--dark-color', '#f8ffff');
         document.documentElement.style.setProperty('--shadow-color', 'rgba(0, 0, 0, 0.5)');
     } else {
         themeToggle.querySelector('i').classList.replace('fa-moon', 'fa-sun');
         document.documentElement.style.setProperty('--white-color', '#f8ffff');
         document.documentElement.style.setProperty('--light-color', '#e0f0f0');
-        document.documentElement.style.setProperty('--dark-color', '#18181f');
+        document.documentElement.style.setProperty('--dark-color', '#282830');
         document.documentElement.style.setProperty('--shadow-color', 'rgba(0, 0, 0, 0.1)');
     }
 };
