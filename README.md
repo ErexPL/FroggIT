e8055e920fd658d72c9ab6009b31a4499ce77dff
ErexPL <zys.dawid@gmail.com>
2025-05-04 13:05:27 +0200

    Fixing hosting

---
+++ README.md
@@ -0,0 +1,3 @@
+# Link do aplikacji
+
+https://froggit.onrender.com
--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.path}`);
+  next();
+});
+
+app.use(express.static(path.join(__dirname, 'public'), {
+  etag: false,
+  maxAge: '0'
+}));
+
+app.use((err, req, res, next) => {
+  console.error('Static file error:', err);
+  next(err);
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
@@ -21,6 +34,17 @@ app.get('/form', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'form.html'));
 });
 
+app.use((req, res) => {
+  console.log(`404: ${req.path} not found`);
+  res.status(404).sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.error('Server error:', err);
+  res.status(500).send('Something broke! Please try again.');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Static files being served from: ${path.join(__dirname, 'public')}`);
 });
